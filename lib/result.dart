84d88d94b73f92144a72679855337cf8774ef037
youssef <youssef#sudoai.codes>
2020-11-21 08:18:06 +0200

    Sampiler-6 T record len

--- lib/result.dart
+++ lib/result.dart
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<result> {
     final anchor = html.document.createElement('a') as html.AnchorElement
       ..href = url
       ..style.display = 'none'
-      ..download = 'some_name.txt';
+      ..download = 'out.txt';
     html.document.body.children.add(anchor);
 
 // download
